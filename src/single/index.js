5d507c83c76b9dd66639bcef7e71af3098fd3a09
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 18:55:41 +0800

    Update index.js

--- src/single/index.js
+++ src/single/index.js
@@ -1,14 +1,4 @@
 import Single from 'rx-single';
 
-import delay from './internal/delay';
-import delaySubscription from './internal/delaySubscription';
-
 export default class SingleEx extends Single {
-  delay(amount, doDelayError, scheduler) {
-    return delay(this, amount, scheduler, doDelayError);
-  }
-
-  delaySubscription(amount, scheduler) {
-    return delaySubscription(this, amount, scheduler);
-  }
 }
